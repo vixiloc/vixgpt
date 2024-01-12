58a71eb92728b913ed88c821e6692672a9fd32a0
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-01-12 23:09:18 +0700

    modify navigations

--- app/src/main/java/com/vixiloc/vixgpt/presentation/navigations/MainNavigationHost.kt
+++ app/src/main/java/com/vixiloc/vixgpt/presentation/navigations/MainNavigationHost.kt
@@ -1,5 +1,7 @@
 package com.vixiloc.vixgpt.presentation.navigations
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -10,10 +12,38 @@ import com.vixiloc.vixgpt.presentation.settings.SettingsScreen
 @Composable
 fun MainNavigationHost(navhostController: NavHostController) {
     NavHost(navController = navhostController, startDestination = MainRoute.Home.name) {
-        composable(MainRoute.Home.name) {
+        composable(
+            route = MainRoute.Home.name,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Down,
+                    animationSpec = tween(300)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Up,
+                    animationSpec = tween(300)
+                )
+            },
+        ) {
             HomeScreen(navhostController = navhostController)
         }
-        composable(MainRoute.Settings.name) {
+        composable(
+            route = MainRoute.Settings.name,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Up,
+                    animationSpec = tween(300)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Down,
+                    animationSpec = tween(300)
+                )
+            },
+        ) {
             SettingsScreen(navhostController = navhostController)
         }
     }
