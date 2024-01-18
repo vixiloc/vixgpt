b89c1c1c3d3650dec29dabe0705981c3e9004114
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-01-18 22:25:44 +0700

    modify Home screen components

--- app/src/main/java/com/vixiloc/vixgpt/presentation/home/components/ChatLists.kt
+++ app/src/main/java/com/vixiloc/vixgpt/presentation/home/components/ChatLists.kt
@@ -1,22 +1,45 @@
 package com.vixiloc.vixgpt.presentation.home.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Card
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import com.vixiloc.vixgpt.domain.model.Chats
 
 @Composable
-fun ChatLists() {
-    Card(modifier = Modifier.fillMaxWidth()) {
-        Text(text = "Question Is basically used to ask the question to each other.")
-        Text(text = "This is the answer of the question, this answer is very long and it will be wrapped to the next line.")
-    }
+fun ChatLists(chats: Chats) {
+    ListItem(
+        headlineContent = {
+            Text(
+                text = chats.question,
+                style = MaterialTheme.typography.headlineMedium,
+                softWrap = true,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        supportingContent = {
+            Text(
+                text = chats.answer,
+                style = MaterialTheme.typography.bodySmall
+            )
+        },
+        colors = ListItemDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.5f),
+            headlineColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            supportingColor = MaterialTheme.colorScheme.onSecondaryContainer,
+        )
+    )
 }
 
-@Preview(showSystemUi = true, showBackground = true)
+@Preview()
 @Composable
 fun ChatListsPreview() {
-    ChatLists()
+//    ChatLists()
 }
